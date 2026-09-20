9fbbefed6901ec914594e0cc41b4fd925a4dbab9
novmar03 <134083781+novmar03@users.noreply.github.com>
2026-09-20 10:38:17 +0300

    Обновить документацию: Регистрация личного кабинета

--- src/components/EditedSection.jsx
+++ src/components/EditedSection.jsx
@@ -1,3 +1,4 @@
+import linkNavigation from './navigation.json';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import englishContent from '../content/editor-pages.en.json?render';
 import React from 'react';
@@ -9,5 +10,7 @@ export default function EditedSection({page,index}) {
  const {i18n:{currentLocale}}=useDocusaurusContext();
  const content=currentLocale==='en'?englishContent:russianContent;
  const html=(content[page]?.segments[index]||'').replaceAll('src="static/img/editor/', 'src="'+base+'img/editor/').replaceAll('__EDITOR_ASSET__/',base+'assets/editor-media/').replace(/href="#\/([^"@]+)(?:@([^"\s]+))?"/g,(_,route,anchor)=>'href="'+base+route+'/'+(anchor?'#'+anchor:'')+'"');
- return <div className="imported-api" onClick={handleCodeCopy} dangerouslySetInnerHTML={{__html:html}}/>;
+ const documentationRoutes=new Set(linkNavigation.flatMap(g=>g.items.map(p=>p.id)));
+ const documentationLinkedHtml=html.replace(/href="\/(?!\/)([^"#]+)(#[^"]*)?"/g,(all,route,hash)=>documentationRoutes.has(route.replace(/^en\//,'').replace(/\/$/,''))?'href="'+base.replace(/\/en\/$/,'/')+route+(hash||'')+'"':all);
+ return <div className="imported-api" onClick={handleCodeCopy} dangerouslySetInnerHTML={{__html:documentationLinkedHtml}}/>;
 }
