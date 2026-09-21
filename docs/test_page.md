d28699a4c6e7c2a782396d8c878e42f8b4979c2e
novmar03 <134083781+novmar03@users.noreply.github.com>
2026-09-21 09:19:27 +0300

    Обновить документацию: Тестовая страница

--- docs/test_page.md
+++ docs/test_page.md
@@ -2,3 +2,8 @@
 title: "Тестовая страница"
 slug: /test_page/
 ---
+
+import EditedSection from '@site/src/components/EditedSection';
+
+<EditedSection page="test_page" index={0} />
+
--- src/components/EditedSection.jsx
+++ src/components/EditedSection.jsx
@@ -1,3 +1,5 @@
+import {useEffect as useCarouselEffect,useRef as useCarouselRef} from 'react';
+import {installCarouselRuntime} from './carousel-runtime';
 import linkNavigation from './navigation.json';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import englishContent from '../content/editor-pages.en.json?render';
@@ -12,5 +14,7 @@ export default function EditedSection({page,index}) {
  const html=(content[page]?.segments[index]||'').replaceAll('src="static/img/editor/', 'src="'+base+'img/editor/').replaceAll('__EDITOR_ASSET__/',base+'assets/editor-media/').replace(/href="#\/([^"@]+)(?:@([^"\s]+))?"/g,(_,route,anchor)=>'href="'+base+route+'/'+(anchor?'#'+anchor:'')+'"');
  const documentationRoutes=new Set(linkNavigation.flatMap(g=>g.items.map(p=>p.id)));
  const documentationLinkedHtml=html.replace(/href="\/(?!\/)([^"#]+)(#[^"]*)?"/g,(all,route,hash)=>documentationRoutes.has(route.replace(/^en\//,'').replace(/\/$/,''))?'href="'+base.replace(/\/en\/$/,'/')+route+(hash||'')+'"':all);
- return <div className="imported-api" onClick={handleCodeCopy} dangerouslySetInnerHTML={{__html:documentationLinkedHtml}}/>;
+ const carouselRoot=useCarouselRef(null);
+ useCarouselEffect(()=>installCarouselRuntime(carouselRoot.current),[html]);
+ return <div ref={carouselRoot} className="imported-api" onClick={handleCodeCopy} dangerouslySetInnerHTML={{__html:documentationLinkedHtml}}/>;
 }
